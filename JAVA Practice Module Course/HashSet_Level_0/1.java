668aff0d31649bf39827a03b195b2e4b1e985adc
Gagan B <gaganb982006@gmail.com>
2026-04-04 18:15:18 +0530

    Create 1.java

---
+++ JAVA Practice Module Course/HashSet_Level_0/1.java
@@ -0,0 +1,11 @@
+/* 1.
+Create an empty `HashSet` of integers.*/
+
+import java.util.HashSet;
+public class Main {
+    public static void main(String[] args) {
+        // Constraint: Use HashSet<Integer>
+        HashSet<Integer> set = new HashSet<>();
+        System.out.println("HashSet created: " + set);
+    }
+}
