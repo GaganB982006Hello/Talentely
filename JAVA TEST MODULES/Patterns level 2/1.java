ff913cab01e182ac43db46a938486c9c555e43f6
Gagan B <gaganb982006@gmail.com>
2026-04-04 18:05:14 +0530

    Add number pattern display program in Java
    Implement a Java program to display a number pattern based on user input.

---
+++ JAVA TEST MODULES/Patterns level 2/1.java
@@ -0,0 +1,26 @@
+'''1.
+Print the following number pattern (print with 2 width space)
+EXAMPLE:
+4
+1 2 3 4
+2 3 4 5
+3 4 5 6
+4 5 6 7'''
+
+
+import java.util.Scanner;
+public class Main {
+    static void displayPattern(int N) {
+        for (int row = 1; row <= N; row++, System.out.println()) {
+            int start = row;
+            for (int col = 1; col <= N; col++) {
+                System.out.printf("%2d ", start++);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        displayPattern(N);
+    }
+}
