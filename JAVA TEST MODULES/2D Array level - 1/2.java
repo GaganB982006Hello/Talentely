542a1115b9b69a292a73d7e3b6606190a8990b48
Gagan B <gaganb982006@gmail.com>
2026-04-04 18:02:19 +0530

    Create 2.java

---
+++ JAVA TEST MODULES/2D Array level - 1/2.java
@@ -0,0 +1,56 @@
+'''2.Given an m x n matrix, return all the elements of the matrix in spiral order.Given an m x n matrix, return all the elements of the matrix in spiral order.'''
+
+import java.util.*;
+
+ class SpiralMatrix {
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return result;
+        }
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+            int[][] matrix = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+            List<Integer> res = spiralOrder(matrix);
+            for (int num : res) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
