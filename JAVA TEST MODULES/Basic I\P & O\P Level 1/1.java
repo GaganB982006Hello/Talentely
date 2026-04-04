c5bc4e0bfed2dc439401ec5d1ab35feb1435617d
Gagan B <gaganb982006@gmail.com>
2026-04-04 18:10:43 +0530

    Create 1.java

---
+++ JAVA TEST MODULES/Basic I\P & O\P Level 1/1.java
@@ -0,0 +1,20 @@
+'''1.
+Write a program that reads two integers from the user and calculates the result of the expression:
+(a + b) * (a - b)'''
+
+
+  import java.util.Scanner;
+class add{
+  public static void main(String[] args){
+    Scanner ip = new Scanner(System.in);
+    int a = ip.nextInt();
+    int b = ip.nextInt();
+    if (a >= -1000 && a <= 1000){
+      if (b >= -1000 && b <= 1000){
+        int c = (a + b) * (a - b);
+        System.out.println(c);
+      }
+    }
+    ip.close();
+  }
+}
