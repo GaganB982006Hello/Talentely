af029dce39183833e80d8d0a8a456b881d931713
Gagan B <gaganb982006@gmail.com>
2026-04-04 20:54:30 +0530

    Create 1.java

---
+++ JAVA Practice Module Course/HashSet_Level_1/1.java
@@ -0,0 +1,16 @@
+/*1.
+Given an array of integers, return the number of unique elements.*/
+
+import java.util.*;
+class Main 
+{ 
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt(); 
+Set<Integer> set = new HashSet<>(); 
+for (int i = 0; i < n; i++)
+set.add(sc.nextInt()); 
+System.out.println(set.size());
+}
+}
