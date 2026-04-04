7c986bf953db02dbb854262c59adbedd5b8b04f2
Gagan B <gaganb982006@gmail.com>
2026-04-04 18:11:27 +0530

    Create 2.java

---
+++ JAVA TEST MODULES/Basic I\P & O\P Level 1/2.java
@@ -0,0 +1,26 @@
+'''2.
+Take three floating-point numbers: a, b, and c, and evaluate the expression:
+(a * b) / (b + c) + (a % c)
+Print the result rounded to 2 decimal places.'''
+  
+
+import java.util.Scanner;
+class decimal{
+  public static void main(String[] args){
+    Scanner ip = new Scanner(System.in);
+    float a = ip.nextFloat();
+    float b = ip.nextFloat();
+    float c = ip.nextFloat();
+    ip.close();
+    if (a >= 0.1 && a <= 1000){
+      if (b >= 0.1 && b <= 1000){
+        if (c >= 0.1 && c <= 1000){
+          if ((b+c) != 0){
+            float res = (a*b) / (b + c) + (a % c);
+            System.out.printf("%.2f", res);
+          }  
+        }
+      }
+    }
+  }
+}  
