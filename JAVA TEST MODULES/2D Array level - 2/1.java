04cf0389dd55a4e276ff71d3d77c322bee0d68e5
Gagan B <gaganb982006@gmail.com>
2026-04-04 18:08:53 +0530

    Create 1.java

---
+++ JAVA TEST MODULES/2D Array level - 2/1.java
@@ -0,0 +1,56 @@
+'''1.
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+
+
+You have to rotate the matrix in-place (i.e., you have to modify the input matrix directly).'''
+
+
+import java.util.*;
+
+public class RotateImage {
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        
+        // First, transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Then, reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCases = 12;
+        for (int t = 0; t < testCases; t++) {
+            int n = sc.nextInt();
+            int[][] matrix = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+            rotate(matrix);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
