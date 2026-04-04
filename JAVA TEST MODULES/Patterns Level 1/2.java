b4ff884a3aba700c78a831f824ed1e293923aac9
Gagan B <gaganb982006@gmail.com>
2026-04-04 18:07:35 +0530

    Add Java program for alternating 1's and 0's pattern
    Implement a Java program to print alternating rows of 1's and 0's based on user input.

---
+++ JAVA TEST MODULES/Patterns Level 1/2.java
@@ -0,0 +1,23 @@
+'''2.
+Print the pattern with Alternate rows of 1's and 0's
+EXAMPLE:
+6
+111111
+000000
+111111
+000000
+111111
+000000'''
+
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        for(int row = 1; row <= num; row++, System.out.println()) {
+            for(int col = 1; col <= num; col++)
+                System.out.print(row % 2);
+        }
+    }
+}
