8778d85962cc390c34b6887e934549f6c7b70b46
Gagan B <gaganb982006@gmail.com>
2026-04-04 17:52:33 +0530

    Create 1.java

---
+++ JAVA TEST MODULES/String level - 1/1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        
+        if (s1.length() != s2.length()) {
+            System.out.println("No");
+        } else {
+            String temp = s1 + s1;
+            if (temp.contains(s2)) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
